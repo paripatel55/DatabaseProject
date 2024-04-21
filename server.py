aa409ec70406af02de1cb9fdaa8109923f27c19c
Steven Lo <143578058+stevloc@users.noreply.github.com>
2024-04-21 02:09:33 -0400

    Update Code

--- server.py
+++ server.py
@@ -1,122 +1,167 @@
-from flask import Flask, render_template, request, redirect
-from flask_mysqldb import MySQL
-from helpers import runstatement, simple_hash, return_table
-from markupsafe import Markup
+from flask import Flask, render_template, request, redirect, url_for
+import pymysql
+import pandas as pd
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "localhost"
-app.config["MYSQL_USER"] = "root"
-app.config["MYSQL_PASSWORD"] = ""
-app.config["MYSQL_DB"] = "project"
+app.config["MYSQL_HOST"] = 'localhost'
+app.config["MYSQL_USER"] = 'root'
+app.config["PASSWORD"] = ""
+app.config["MY_SQLDB"] = 'criminal'
 
-mysql = MySQL(app)
+from helpers import runstatement, simple_hash, return_table
+from markupsafe import Markup
+def get_db_connection():
+    return pymysql.connect(host=app.config['MYSQL_HOST'],
+                           user=app.config['MYSQL_USER'],
+                           password=app.config['PASSWORD'],
+                           db=app.config['MY_SQLDB'])
 
+def runstatement(statement):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("Start TRANSACTION;") #Transaction
+    cursor.execute(statement)
+    results = cursor.fetchall()
+    df = pd.DataFrame()  
+    if results and cursor.description: 
+        column_names = [desc[0] for desc in cursor.description]
+        df = pd.DataFrame(results, columns=column_names) 
+    conn.commit() #Commit
+    cursor.close()
+    conn.close() 
+    return df
 
-@app.route("/") # first page the user will see is login
+def check_existance(db, attr_selection, attr_value):
+    try:
+        db = runstatement(f"SELECT * FROM {db}")
+    except Exception:
+        return False
+    for i, j in db.iterrows():
+        if str(j[attr_selection]) == str(attr_value):
+            return True
+    return False
+
+@app.route("/")  # First page the user will see is login 
 def login_page():
-    return render_template("login.html")
+    return render_template("Officers/Update_Officer.html")
 
-@app.route("/login_pressed", methods = ["POST"])
-# route to a page that handles when the login button is pressed
-def login_pressed():
-    # get the username and password from frontend
-    username = request.form.get("username")
-    password = request.form.get("password")  
+# This file only has Update functions!!
+# Starts Here
 
-    # sql statement to check if user and password match up
-    authenticate_login = f"SELECT user_type from users WHERE username='{username}' AND password='{simple_hash(password)}'"
-    df_output = runstatement(authenticate_login, mysql) # get output as dataframe
-    if (not df_output.empty): # check if nothing was returned(authentication failed)
-        user = df_output.loc[0][0] # get the user type
-        return redirect(f"/home_{user}") # redirect to home page for that user type
-    return login_page() # otherwise if authentication failed, stay at login page
-
-@app.route("/signup")
-# a page where a new user and register in the database
-def signup_page():
-    return render_template("signup.html")
-
-@app.route("/signup_pressed", methods = ["POST"]) # handle when the "submit" button is pressed
-def signup_pressed():
-    # get info from front end
-    username = request.form.get("username")
-    password = request.form.get("password")   
-    criminal_select = request.form.get("criminal_select")
-    user_type = "Criminal" if criminal_select else "Officer"
-    id = request.form.get("ID")
-
-    # based on which type of user they are our search will change
-    token = "criminal_id" if criminal_select else "officer_id"
-    table = "criminal" if criminal_select else "officer"
-
-    # statement to check if they are already in the database
-    check_existance_in_database = f"SELECT {token} from {table} WHERE {token}={id}"
-    # statement to check if they already have an accoount
-    check_existance_in_user_table = f"SELECT id from users WHERE id={id}"
-
-    if (not runstatement(check_existance_in_database, mysql).empty 
-        and runstatement(check_existance_in_user_table, mysql).empty):
-        # if they are in the db and are not already registered, add them to the user table
-        insert_user_statment = f"INSERT INTO users VALUES('{username}','{simple_hash(password)}', '{id}', '{user_type}')"
-        runstatement(insert_user_statment, mysql)
-    else: # otherwise keep them at the sign up page
-        return signup_page()
-    
-    # if everything works out fine, redirect to login page
-    return redirect("/")
-
-@app.route('/home_<user>')
-def home(user):
-    if (user == "Officer"):
-        return render_template("Officer/officer_home.html") # render officer home page
-        
-    elif (user == "Criminal"):
-        ... # reder criminal home page
-
-    return "hi"
-
-@app.route('/Search_Criminal')
-def Search_Criminal(extra_rows=""):
-    return render_template('Officer/Search_Criminal.html', extra_rows=Markup(extra_rows))
-
-@app.route('/Search_Pressed', methods = ["POST"])
-def Search_Pressed():
-    first_name = request.form.get("first_name")
-    last_name = request.form.get("last_name")
-    filter_attr = request.form.get("items")
+@app.route('/Update_Criminal', methods=["POST"])
+def Update_Criminal():
+    return render_template('Officer/Update_Criminal.html')
+            
+@app.route('/Update_Criminal_Pressed', methods=["POST"])
+def Update_Criminal_Pressed():
+    Criminal_ID = request.form.get("Criminal_ID")
     attr_value = request.form.get("attr_value")
-    if (filter_attr != "None"):
-        search_statment = f"SELECT * from criminal WHERE first = '{first_name}' AND last = '{last_name}' AND {filter_attr}='{attr_value}'"
-    else:
-        search_statment = f"SELECT * from criminal WHERE first = '{first_name}' AND last = '{last_name}'"
+    attr_selection = request.form.get("attr_selection")
+    statement = f"UPDATE Criminal SET {attr_selection} = '{attr_value}' WHERE Criminal_ID = {Criminal_ID};"
+    try: # If it fails, it'd go back to the original page
+        runstatement(statement) 
+    except Exception:
+        return Update_Criminal()
+    return Update_Criminal()
+
+@app.route('/Update_Officer', methods=["POST"])
+def Update_Officer():
+    return render_template('Officer/Update_Officer.html')
+            
+@app.route('/Update_Officer_Pressed', methods=["POST"])
+def Update_Officer_Pressed():
+    Officer_ID = request.form.get("Officer_ID")
+    attr_value = request.form.get("attr_value")
+    attr_selection = request.form.get("attr_selection")
+    statement = f"UPDATE Officer SET {attr_selection} = '{attr_value}' WHERE Officer_ID = {Officer_ID};"
+    try: # If it fails, it'd go back to the original page
+        runstatement(statement) 
+    except Exception:
+        return Update_Officer()
+    return Update_Officer()
+
+@app.route('/Update_Alias', methods=["POST"])
+def Update_Alias():
+    return render_template('Officer/Update_Alias.html')
+
+
+@app.route('/Update_Alias_Pressed', methods=["POST"])
+def Update_Alias_Pressed():
+    Alias_ID = request.form.get("Alias_ID")
+    attr_value = request.form.get("attr_value")
+    attr_selection = request.form.get("attr_selection")
+
+    if attr_selection == "Criminal_ID":
+        if not (check_existance("Criminal", attr_selection, attr_value)):
+            return Update_Alias()
+
+    statement = f"UPDATE Alias SET {attr_selection} = '{attr_value}' WHERE Alias_ID = {Alias_ID};"
+    try: # If it fails, it'd go back to the original page
+        runstatement(statement) 
+    except Exception:
+        return Update_Alias()
+    return Update_Alias()
+
+@app.route('/Update_Appeals', methods=["POST"])
+def Update_Appeals():
+    return render_template('Officer/Update_Appeals.html')
+            
+@app.route('/Update_Appeals_Pressed', methods=["POST"])
+def Update_Appeals_Pressed():
+    Appeals_ID = request.form.get("Appeals_ID")
+    attr_value = request.form.get("attr_value")
+    attr_selection = request.form.get("attr_selection")
+    attr_value_date = request.form.get("attr_value_date")
+    attr_selection_date = request.form.get("attr_selection_date")
+
+    if attr_selection == "Crime_ID":
+        if not (check_existance("Crime", attr_selection, attr_value)):
+            return Update_Appeals()
+        
+    if attr_selection != 'None' and attr_selection_date != 'None':
+        statement = f"UPDATE Appeals SET {attr_selection} = '{attr_value}', {attr_selection_date} = '{attr_value_date}' WHERE Appeal_ID = {Appeals_ID};"
+    elif attr_selection_date != 'None':
+        statement = f"UPDATE Appeals SET {attr_selection_date} = '{attr_value_date}' WHERE Appeal_ID = {Appeals_ID};"
+    elif attr_selection != 'None':
+        statement = f"UPDATE Appeals SET {attr_selection} = '{attr_value}' WHERE Appeal_ID = {Appeals_ID};"
+    try: # If it fails, it'd go back to the original page
+        runstatement(statement) 
+        print(statement)
+    except Exception:
+        return Update_Appeals()
+    return Update_Appeals()
+
+@app.route('/Update_Crime_Charges', methods=["POST"])
+def Update_Crime_Charges():
+    return render_template('Officer/Update_Crime_Charges.html')
+            
+@app.route('/Update_Crime_Charges_Pressed', methods=["POST"])
+def Update_Crime_Charges_Pressed():
+    Charge_ID = request.form.get("Charge_ID")
+    attr_value = request.form.get("attr_value")
+    attr_selection = request.form.get("attr_selection")
+    attr_value_date = request.form.get("attr_value_date")
+    attr_selection_date = request.form.get("attr_selection_date")
     
-    df_output = runstatement(search_statment, mysql)
-    if (df_output.empty):
-        redirect("/Search_Criminal")
-        return Search_Criminal(return_table(df_output))
-    else:
-        redirect("/Search_Criminal")
-        return Search_Criminal(return_table(df_output))
+    if attr_selection == "Crime_ID":
+        if not (check_existance("Crime", attr_selection, attr_value)):
+            return Update_Crime_Charges()
+    if attr_selection == "Crime_code":
+        if not (check_existance("Crime_code", attr_selection, attr_value)):
+            return Update_Crime_Charges()
 
-@app.route('/Search_Alias', )
-def Search_Alias():
-    return render_template('Officer/Search_Alias.html')
-
-@app.route('/Search_Prob_Officer')
-def Search_Prob_Officer():
-    return render_template('Officer/Search_Prob_Officer.html')
-
-
-# app.add_url_rule("/login_pressed", view_func=login_pressed)
-# app.add_url_rule("/signup_pressed", view_func=signup_pressed)
-
+    if attr_selection != 'None' and attr_selection_date != 'None':
+        statement = f"UPDATE Crime_charges SET {attr_selection} = '{attr_value}', {attr_selection_date} = '{attr_value_date}' WHERE Charge_ID = {Charge_ID};"
+    elif attr_selection_date != 'None':
+        statement = f"UPDATE Crime_charges SET {attr_selection_date} = '{attr_value_date}' WHERE Charge_ID = {Charge_ID};"
+    elif attr_selection != 'None':
+        statement = f"UPDATE Crime_Charges SET {attr_selection} = '{attr_value}' WHERE Charge_ID = {Charge_ID};"
+    try: # If it fails, it'd go back to the original page
+        runstatement(statement) 
+    except Exception:
+        return Update_Crime_Charges()
+    return Update_Crime_Charges()
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-# types of queries
-    # one for Select __ where attrr = __
-    # one for select __ where attr like "___%"
-
-
 
